3099fa6bd7eaff889ff4289bd15eab251b26b28d
Willian Gomes <will7seven@gmail.com>
2020-02-06 11:31:49 -0300

    implementação da rotina de inicialização

--- code/main.asm
+++ code/main.asm
@@ -21,16 +21,45 @@ Start:
 	clrf	ANSEL		; configure all PORTA,
 						; pins as digital I/O
 Main:
-	goto 	RotinaInicializacao
+	call	RotinaInicializacao
 	
 RotinaInicializacao:
 	bcf 	STATUS, RP1
 	bcf 	STATUS, RP0 ; change to bank0
 	movlw 	0x0F			
 	movwf 	PORTA		; set pins RA0-RA3
-	call	Delay_1s	; call delay function
-	clrf	PORTA		; reset pins RA0-RA3		
+	call	Delay_1s	; call delay function			
+	clrf 	led_cnt		; led_cnt = 0
 	
-						
+LedCountLoop:
+	clrf	PORTA		; clear pins RA0-RA3
+	movlw 	.0
+	subwf 	led_cnt, W
+	btfsc	STATUS, Z	; led_cnt=0?
+	bsf 	PORTA, RA0	; yes
 	
+	movlw 	.1
+	subwf 	led_cnt, W
+	btfsc	STATUS, Z	; led_cnt=1?
+	bsf 	PORTA, RA1	; yes
+					
+	movlw 	.2
+	subwf 	led_cnt, W
+	btfsc	STATUS, Z	; led_cnt=1?
+	bsf 	PORTA, RA2	; yes
+	
+	movlw 	.3
+	subwf 	led_cnt, W
+	btfsc	STATUS, Z	; led_cnt=1?
+	bsf 	PORTA, RA3	; yes
+	
+	call 	Delay_200ms								
+	incf	led_cnt, F 	; incrementa led_cnt
+	
+	movlw	.4
+	subwf	led_cnt, W	
+	btfss 	STATUS, Z 	; led_cnt=4?
+	goto	LedCountLoop		; no
+	clrf	PORTA		; yes
+	return
 	
